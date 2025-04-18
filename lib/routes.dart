0f6743424a4ba3a0d032e9f72a1f2aadabcc6b70
Macbook Pro 2017 <macbook@Macbooks-MacBook-Pro.local>
2025-04-19 00:26:35 +0700

    transaction page - not integrated with firebase

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,8 +2,8 @@ import 'package:get/get.dart';
 import 'pages/start.dart';
 import 'pages/homepage.dart';
 import 'pages/login.dart';
-/* import 'pages/settings.dart';
-import 'pages/transaction.dart'; */
+import 'pages/settings.dart';
+import 'pages/transaction.dart';
 import 'pages/signup.dart';
 
 class AppRoutes {
@@ -18,6 +18,7 @@ class AppRoutes {
     GetPage(name: start, page: () => StartPage()),
     GetPage(name: login, page: () => LoginPage()),
     GetPage(name: home, page: () => HomePage()),
+    GetPage(name: transaction, page: () => TransaksiPage()),
     GetPage(name: signup, page: () => SignUp()),
   ];
 }
