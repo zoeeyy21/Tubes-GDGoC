1dbb10beb3f7e416465ace00dd6829d6ed8e3036
zoeeyy21 <superspeeedy980@gmail.com>
2025-04-19 09:39:12 +0700

    membuat transaction page dan  settings

--- lib/routes.dart
+++ lib/routes.dart
@@ -11,7 +11,7 @@ class AppRoutes {
   static const home = '/home';
   static const login = '/login';
   static const signup = '/signup';
-  static const settings = '/settings';
+  static const settings = '/pengaturan';
   static const transaction = '/transaction';
 
   static final routes = [
@@ -19,6 +19,7 @@ class AppRoutes {
     GetPage(name: login, page: () => LoginPage()),
     GetPage(name: home, page: () => HomePage()),
     GetPage(name: transaction, page: () => TransaksiPage()),
+    GetPage(name: settings, page: () => SettingsPage()),
     GetPage(name: signup, page: () => SignUp()),
   ];
 }
